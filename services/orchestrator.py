e7bf9d2c884f9720df68bd14f968c07ac8fec72c
Sanjay G <gokulesh2615@gmail.com>
2026-05-31 14:12:51 +0530

    feat(orchestrator): implement service orchestration layer

---
+++ services/orchestrator.py
@@ -0,0 +1,16 @@
+from packages.core import InferenceEngine, TaskScheduler, TransactionalMemoryStore
+import logging
+
+logger = logging.getLogger(__name__)
+
+class Orchestrator:
+    def __init__(self, llm_type):
+        self.inference_engine = InferenceEngine(llm_type)
+        self.task_scheduler = TaskScheduler()
+        self.memory_store = TransactionalMemoryStore()
+
+    def execute_task(self, task_id, task_input):
+        self.task_scheduler.schedule_task(task_id, task_input)
+        self.inference_engine.execute_task(task_id, task_input)
+        task_result = self.inference_engine.get_task_result(task_id)
+        self.memory_store.write(task_id, task_result)
